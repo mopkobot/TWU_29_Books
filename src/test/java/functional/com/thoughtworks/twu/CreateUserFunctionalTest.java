3b277c02ea12372b7aa749be1571ce9323c9c650
xiasiyu@gmail.com <xiasiyu@gmail.com>
2012-11-05 10:19:45 +0530

    Summer & Akshay: Remove view-book functional test from create-user functional test

--- src/test/java/functional/com/thoughtworks/twu/CreateUserFunctionalTest.java
+++ src/test/java/functional/com/thoughtworks/twu/CreateUserFunctionalTest.java
@@ -1,7 +1,6 @@
 package functional.com.thoughtworks.twu;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -36,46 +35,4 @@ public class CreateUserFunctionalTest {
         webDriver.findElement(By.className("btn-submit")).click();
     }
 
-    public static class ViewBookFunctionalTest{
-        private WebDriver webDriver;
-        @Before
-        public void setUp() {
-            webDriver = new FirefoxDriver();
-        }
-        @Test
-        public void shouldValidateAllElementsPresent(){
-            login();
-            webDriver.get("http://localhost:8080/twu/viewbook?booktitle=The Casual Vacancy");
-            Assert.assertEquals(webDriver.findElement(By.className("book-cover")).isDisplayed(), true);
-            Assert.assertEquals(webDriver.findElement(By.className("title")).isDisplayed(), true);
-            Assert.assertEquals(webDriver.findElement(By.className("author")).isDisplayed(), true);
-            Assert.assertEquals(webDriver.findElement(By.className("isbn")).isDisplayed(), true);
-            Assert.assertEquals(webDriver.findElement(By.className("description")).isDisplayed(), true);
-        }
-
-        private void login() {
-            webDriver.get("http://localhost:8080/twu");
-            webDriver.findElement(By.id("username")).sendKeys("test.twu");
-            webDriver.findElement(By.id("password")).sendKeys("Th0ughtW0rks@12");
-            webDriver.findElement(By.className("btn-submit")).click();
-        }
-
-        @Test
-        public void shouldDisplayBookNotFoundWhenBookIsNotPresent(){
-            login();
-
-            webDriver.get("http://localhost:8080/twu/viewbook?booktitle=alkdhaksdh");
-            Assert.assertEquals("Could not find book", webDriver.findElement(By.className("title")).getText());
-        }
-
-        @Test
-        public void shouldShowTheSameBook() {
-            login();
-            webDriver.get("http://localhost:8080/twu/viewbook?booktitle=alkdhaksdh");
-            String title1= webDriver.findElement(By.className("title")).getText();
-            webDriver.get("http://localhost:8080/twu/viewbook?booktitle=alkdhaksdh");
-            String title2 =webDriver.findElement(By.className("title")).getText();
-            Assert.assertEquals(title1, title2);
-        }
-    }
 }
