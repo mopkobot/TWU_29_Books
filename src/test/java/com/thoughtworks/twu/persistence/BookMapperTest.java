da4b1697f7c05c62a53c263dc9e6c7fa52cc86c7
Aleksandar Serafimoski <aserafimoski@gmail.com>
2012-11-05 12:23:46 +0530

    #145 Alex/Saumya/Charlene - Added transactional rollback on top of the class

--- src/test/java/com/thoughtworks/twu/persistence/BookMapperTest.java
+++ src/test/java/com/thoughtworks/twu/persistence/BookMapperTest.java
@@ -3,15 +3,21 @@ package com.thoughtworks.twu.persistence;
 import com.thoughtworks.twu.domain.Book;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
+
 //Ensure that BookMapper could interact with DB as expected
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
 @TransactionConfiguration(defaultRollback=true)
-public class BookMapperTest extends IntegrationTest {
+public class BookMapperTest{
     @Autowired
     private BookMapper bookMapper;
     private Book book;
