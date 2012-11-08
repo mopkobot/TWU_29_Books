d526c65a1f7140037d449b0f10371a0863296ea8
Charlene <charlet@thoughtworks.com>
2012-11-08 09:46:34 +0530

    [#145] Charlene: Refactored ViewBookFunctionalTest to use dummy data

--- src/test/java/functional/com/thoughtworks/twu/ViewBookFunctionalTest.java
+++ src/test/java/functional/com/thoughtworks/twu/ViewBookFunctionalTest.java
@@ -1,56 +1,54 @@
 package functional.com.thoughtworks.twu;
+
+
+import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.internal.matchers.StringContains.containsString;
 
 public class ViewBookFunctionalTest{
     private WebDriver webDriver;
+
     @Before
     public void setUp() {
         webDriver = new FirefoxDriver();
-    }
-    @Ignore
-    public void shouldValidateAllElementsPresent(){
         login();
-        webDriver.get("127.0.0.1:8080/twu/viewbook?booktitle=The Casual Vacancy");
-        assertOnBookTitle();
-        assertOnBookCover();
-        assertOnBookDescription();
-        assertOnIsbn();
-        assertOnAuthor();
-    }
-    private void assertOnIsbn() {
-
-        assertEquals(true, webDriver.findElement(By.className("isbn")).isDisplayed());
     }
 
-    private void assertOnAuthor() {
-        assertEquals(true, webDriver.findElement(By.className("author"))
-                .isDisplayed());
-    }
-    private void assertOnBookTitle() {
-        WebElement titleElement = locateElement("h1.title");
-        assertEquals("The Casual Vacancy", titleElement.getText());
+
+    @Test
+    public void shouldUnderstandViewBookPageWhenAllInformationIsPresent(){
+        goToURL("127.0.0.1:8080/twu/viewbook?booktitle=Lavanya and sanchari QAs");
+
+        assertOnBookTitle("Lavanya and sanchari QAs");
+        assertOnBookCover("http://ecx.images-amazon.com/images/I/51HVlrefdkL._SL500_AA300_.jpg");
+        assertOnAuthor("J.K. Rowling");
+        assertOnIsbn("1234567890");
+        assertOnBookDescription("The books chronicle the adventures of a wizard");
     }
 
-    private void assertOnBookCover() {
-        WebElement imageElement= locateElement("img.book-img");
-        assertThat(imageElement.getAttribute("src").isEmpty(), is(false));
+    @Test
+    public void shouldUnderstandViewBookPageWhenNotAllInformationIsPresent(){
+        goToURL("127.0.0.1:8080/twu/viewbook?booktitle=When devs are not coding");
+
+        assertOnBookTitle("When devs are not coding");
+        assertOnBookCover("http://127.0.0.1:8080/twu/static/images/default_image.gif");
+        assertOnAuthor("Benjamin Parzybok");
+        assertOnIsbn("8947321890");
+        assertOnBookDescription("No description available");
     }
 
-    private void assertOnBookDescription() {
-        WebElement descriptionElement = locateElement("section.description");
-        assertThat(descriptionElement.getText().isEmpty(), is(false));
-    }
-    private WebElement locateElement(String selector){
-        return webDriver.findElement(By.cssSelector(selector));
+    @Test
+    public void shouldDisplayBookNotFoundWhenBookIsNotPresent(){
+        goToURL("127.0.0.1:8080/twu/viewbook?booktitle=alkdhaksdh");
+        assertOnBookTitle("Could not find book");
     }
 
     private void login() {
@@ -60,28 +58,41 @@ public class ViewBookFunctionalTest{
         webDriver.findElement(By.className("btn-submit")).click();
     }
 
-    @Ignore
-    public void shouldDisplayBookNotFoundWhenBookIsNotPresent(){
-        login();
-
-        webDriver.get("127.0.0.1:8080/twu/viewbook?booktitle=alkdhaksdh");
-        assertEquals("Could not find book",webDriver.findElement(By.className("title")).getText());
+    private void goToURL(String url) {
+        webDriver.get(url);
     }
 
-    @Ignore
-    public void shouldShowTheSameBook() {
-        login();
-        webDriver.get("127.0.0.1:8080/twu/viewbook?booktitle=The Casual Vacancy");
-        String title1= webDriver.findElement(By.className("title")).getText();
-        webDriver.get("127.0.0.1:8080/twu/viewbook?booktitle=The Casual Vacancy");
-        String title2 =webDriver.findElement(By.className("title")).getText();
-        assertEquals(title1,title2);
+    private void assertOnBookTitle(String expectedTitle) {
+        WebElement titleElement = locateElement("h1.title");
+        assertThat(titleElement.getText(), is(expectedTitle));
     }
-    @Ignore
-    public void shouldShowBookEvenIfItHasSameName(){
-        login();
-        webDriver.get("127.0.0.1:8080/twu/viewbook?booktitle=The Book Of The Dead");
-        String title= webDriver.findElement(By.className("title")).getText();
-        assertEquals("The Book Of The Dead",title);
+
+    private void assertOnBookCover(String expectedCover) {
+        WebElement imageElement= locateElement("img.book-img");
+        assertThat(imageElement.getAttribute("src"), is(expectedCover));
+    }
+
+    private void assertOnBookDescription(String expectedDescription) {
+        WebElement descriptionElement = locateElement("section.description");
+        assertThat(descriptionElement.getText(), containsString(expectedDescription));
+    }
+
+    private void assertOnIsbn(String expectedIsbn) {
+        WebElement isbnElement = locateElement("div.isbn");
+        assertThat(isbnElement.getText(), containsString(expectedIsbn));
+    }
+
+    private void assertOnAuthor(String expectedAuthor) {
+        WebElement authorElement = locateElement("h2.author");
+        assertThat(authorElement.getText(), is(expectedAuthor));
+    }
+
+    private WebElement locateElement(String selector){
+        return webDriver.findElement(By.cssSelector(selector));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        webDriver.close();
     }
 }
