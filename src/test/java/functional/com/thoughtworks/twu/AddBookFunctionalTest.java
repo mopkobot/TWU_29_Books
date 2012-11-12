595c18b14b818bea6cc6781f72aa5965a7b6be67
lavanyam <lavanyam.210@gmail.com>
2012-11-12 13:33:23 +0530

    Lavanya: Refactored add book functional test

--- src/test/java/functional/com/thoughtworks/twu/AddBookFunctionalTest.java
+++ src/test/java/functional/com/thoughtworks/twu/AddBookFunctionalTest.java
@@ -16,39 +16,18 @@ public class AddBookFunctionalTest {
     public void setUp() {
         webDriver = new HtmlUnitDriver();
         CommonSteps.login(webDriver,"test.twu","Th0ughtW0rks@12");
-        enterUserName();
-        searchBook();
+        CommonSteps.saveProfileInformation(webDriver);
+        CommonSteps.searchBook(webDriver,"9780316228534","searchByISBN");
     }
 
     @Test
     public void shouldRedirectToViewBookPageByClick() {
         webDriver.findElement(By.className("add-book")).click();
-
         assertTrue(webDriver.findElement(By.tagName("title")).getText().contains("ReaderFeeder"));
     }
 
     @After
     public void tearDown(){
         webDriver.close();
-    }
-
-    private void login() {
-        webDriver.get("http://127.0.0.1:8080/twu");
-        webDriver.findElement(By.id("username")).sendKeys("test.twu");
-        webDriver.findElement(By.id("password")).sendKeys("Th0ughtW0rks@12");
-        webDriver.findElement(By.className("btn-submit")).click();
-    }
-
-    private void enterUserName() {
-        webDriver.get("http://127.0.0.1:8080/twu");
-        webDriver.findElement(By.name("username")).sendKeys("barbie");
-        webDriver.findElement(By.name("submit")).click();
-    }
-
-    private void searchBook() {
-        webDriver.get("http://127.0.0.1:8080/twu/search_book");
-        webDriver.findElement(By.name("searchValue")).sendKeys("9780316228534");
-        webDriver.findElement(By.id("searchByISBN")).click();
-        webDriver.findElement(By.id("search")).submit();
     }
 }
